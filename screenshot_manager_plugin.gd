a80856225d93d3e166c3c3a014cb3a791e5b39b6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-17 09:57:03 -0800

    Run `gdformat . --line-length 180`

--- screenshot_manager_plugin.gd
+++ screenshot_manager_plugin.gd
@@ -21,9 +21,7 @@ func _get_plugin_name() -> String:
 func _enter_tree() -> void:
 	editor_interface = get_editor_interface()
 
-	add_autoload_singleton(
-		"ScreenshotManager", "res://addons/sar1_screenshot_manager/screenshot_manager.gd"
-	)
+	add_autoload_singleton("ScreenshotManager", "res://addons/sar1_screenshot_manager/screenshot_manager.gd")
 
 
 func _exit_tree() -> void:
